ab810e0ad5f8c8db3e251026cfc53de2fa5f1c69
itpark-team <46758840+itpark-team@users.noreply.github.com>
2022-06-12 10:24:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppTestOpeartorov/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestOpeartorov
+{
+    internal class Point
+    {
+        private int _x;
+        private int _y;
+
+        public static int a = 0;
+
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int X
+        {
+            get { return _x; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+        }
+
+        public static Point operator+(Point point1, Point point2)
+        {
+            return new Point(point1.X + point2.X, point1.Y + point2.Y);
+        }
+    }
+}
---
+++ ConsoleAppTestOpeartorov/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestOpeartorov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point(1, 1);
+            Point point2 = new Point(2, 3);
+
+            Point point3 = point1 + point2;
+
+            Console.ReadKey();
+        }
+    }
+}
